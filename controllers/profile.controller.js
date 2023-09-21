52468b551e4f7227e9c2b0f38648832075496978
gamitej <gamitejpratapsingh998@gmail.com>
2023-09-21 17:18:17 +0530

    profile api

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,6 +1,7 @@
 const Solved = require("../models/Solved");
 const Questions = require("../models/Questions");
-const { tableData } = require("../data/dummyData");
+const { sortBy } = require("lodash");
+const nameMapping = require("../utils/nameMapping.json");
 
 // ============== USER STATUS DATA ===============
 
@@ -43,10 +44,10 @@ const getUserStatus = async (req, res) => {
     const response = {
       easySolved: levelMapping.easy.solved,
       easyTotal: levelMapping.easy.total,
-      hardSolved: levelMapping.medium.solved,
-      hardTotal: levelMapping.medium.total,
-      mediumSolved: levelMapping.hard.solved,
-      mediumTotal: levelMapping.hard.total,
+      mediumSolved: levelMapping.medium.solved,
+      mediumTotal: levelMapping.medium.total,
+      hardSolved: levelMapping.hard.solved,
+      hardTotal: levelMapping.hard.total,
       total,
       totalSolved,
     };
@@ -62,7 +63,36 @@ const getUserStatus = async (req, res) => {
 
 const getTableData = async (req, res) => {
   try {
-    return res.status(200).json({ data: tableData, error: false });
+    // request
+    const { id } = req.query;
+
+    if (!id)
+      return res.status(400).json({ data: "id is required", error: true });
+
+    // data from db
+    const questions = await Questions.find();
+    const solvedQuestions = await Solved.find({ userId: id });
+
+    // convert solvedQuestions to set of questions id
+    const solvedQuestionIds = new Set(
+      solvedQuestions.map((solved) => solved.quesId)
+    );
+
+    // data manipulation
+    const response = questions.map(
+      ({ url, topic, level, questionName, createdAt, _id }) => ({
+        date: createdAt,
+        done: solvedQuestionIds.has(_id.toString()) ? "Yes" : "No",
+        url,
+        topic: nameMapping[topic],
+        level,
+        question: questionName,
+      })
+    );
+
+    const sortedResponse = sortBy(response, "date");
+
+    return res.status(200).json({ data: { rows: response }, error: false });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ data: "Something Went Wrong!", error: true });
